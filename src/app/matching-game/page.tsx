9121a0eb21e58a5a257f1a35ad114f5a383ede95
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-01-14 14:15:31 -0500

    Added deck shuffling

--- src/app/matching-game/page.tsx
+++ src/app/matching-game/page.tsx
@@ -11,13 +11,18 @@ import {
 import Header from "../components/header";
 import { Flashcard } from "@prisma/client";
 
+type MatchingCard = {
+    id: string,
+    field: string
+}
+
 export default function MatchingGame() {
-    const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+    const [flashcards, setFlashcards] = useState<MatchingCard[]>([]);
     const [matchedCards, setMatchedCards] = useState<Flashcard[]>([{ question: '', answer: '' } as Flashcard]);
     const [possibleMatches, setPossibleMatches] = useState({ question: "", answer: "" });
     const [sum, setSum] = useState(0);
 
-    const handleSum = (field: string) => {
+    const handleSum = (field: string, id: number) => {
         let num = sum + 1;
         setSum(num);
         console.log(num);
@@ -30,46 +35,72 @@ export default function MatchingGame() {
         if (num === 2) {
             possibleMatches.answer = field;
             console.log(possibleMatches);
-            handleMatch();
+            //handleMatch();
         }
     }
 
-    const handleMatch = () => {
-        const match = flashcards.find((flashcard) => {
-            console.log(flashcard);
-            console.log(flashcard.question);
-            console.log(flashcard.answer);
-            console.log(possibleMatches.question);
-            console.log(possibleMatches.answer);
-            if (flashcard.question === possibleMatches.question && flashcard.answer === possibleMatches.answer
-                || flashcard.answer === possibleMatches.question && flashcard.question === possibleMatches.answer) {
-                    return flashcard;
-                }
-        })
+    // const handleMatch = () => {
+    //     const match = flashcards.find((flashcard) => {
+    //         console.log(flashcard);
+    //         console.log(flashcard.question);
+    //         console.log(flashcard.answer);
+    //         console.log(possibleMatches.question);
+    //         console.log(possibleMatches.answer);
+    //         if (flashcard.question === possibleMatches.question && flashcard.answer === possibleMatches.answer
+    //             || flashcard.answer === possibleMatches.question && flashcard.question === possibleMatches.answer) {
+    //                 return flashcard;
+    //             }
+    //     })
 
-        if (match) {
-            const updatedFlashcards = [...matchedCards, 
+    //     if (match) {
+    //         const updatedFlashcards = [...matchedCards, 
 
-                {   
-                    id: match.id, 
-                    question: match.question, 
-                    answer: match.answer,
-                    createdAt: match.createdAt,
-                    flashcardDeckID: match.flashcardDeckID
+    //             {   
+    //                 id: match.id, 
+    //                 question: match.question, 
+    //                 answer: match.answer,
+    //                 createdAt: match.createdAt,
+    //                 flashcardDeckID: match.flashcardDeckID
 
-                }
-            ];
-            setMatchedCards(updatedFlashcards);
+    //             }
+    //         ];
+    //         setMatchedCards(updatedFlashcards);
 
-            const currentFlashcards = [...flashcards];
-            const newFlashcards = currentFlashcards.filter((flashcard) => flashcard !== match);
-            setFlashcards(newFlashcards);
-            console.log("Match");
-            setSum(0);
-        } else {
-            console.log("No Match");
-            setSum(0);
+    //         const currentFlashcards = [...flashcards];
+    //         const newFlashcards = currentFlashcards.filter((flashcard) => flashcard !== match);
+    //         setFlashcards(newFlashcards);
+    //         console.log("Match");
+    //         setSum(0);
+    //     } else {
+    //         console.log("No Match");
+    //         setSum(0);
+    //     }
+    // }
+    
+    const shuffleCards = (cards: Flashcard[]) => {
+        const shuffle = (array: any[]) => {
+            const newArray = [...array];
+
+            for (let i = newArray.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+            }
+
+            return newArray;
         }
+
+        const shuffledQuestions = shuffle(cards);
+        const questions: MatchingCard[] = shuffledQuestions.map((card) => ({ id: card.id, field: card.question }));
+        console.log(questions)
+
+        const shuffledAnswers = shuffle(cards);
+        const answers: MatchingCard[] = shuffledAnswers.map((card) => ({ id: card.id, field: card.answer }));
+        console.log(answers)
+
+        const shuffledDeck: MatchingCard[] = shuffle(questions.concat(answers));
+        console.log(shuffledDeck);
+
+        return shuffledDeck;
     }
 
     const getFlashcards = async() => {
@@ -94,19 +125,16 @@ export default function MatchingGame() {
     (async () => { 
         const flashcardDeck = await getFlashcards();
         console.log("flashcards", flashcardDeck);
+        const shuffledDeck = shuffleCards(flashcardDeck);
+        console.log(shuffledDeck);
 
         if (!flashcards) {
             console.log("can't get cards");
         }
 
-        setFlashcards(
-            flashcardDeck.map((flashcard: Flashcard) => ({
-                question: flashcard.question,
-                answer: flashcard.answer
-            }))
-        )
+        setFlashcards(shuffledDeck);
 
-        setMatchedCards(flashcardDeck.map(() => ({ question: '', answer: ' ' } as Flashcard)));
+        //setMatchedCards(flashcardDeck.map(() => ({ question: '', answer: ' ' } as Flashcard)));
     })()
     }, []);
 
@@ -116,22 +144,12 @@ export default function MatchingGame() {
             <div className="grid grid-cols-4 gap-4 mt-12">
                 {flashcards.map((flashcard, index) => (
                     <div 
-                        className="w-60 h-60 rounded-lg border-primary_blue border-4 flex justify-center items-center cursor-pointer hover:bg-gray-400 duration-300 focus:bg-purple-700 duration-100" 
+                        className="w-60 h-60 rounded-lg border-primary_blue border-4 flex justify-center items-center cursor-pointer hover:bg-gray-400 duration-300" 
                         key={index}
-                        onClick={() => handleSum(flashcard.question)}
+                        onClick={() => handleSum(flashcard.field, index)}
                     >
-                        {flashcard.question}
+                        {flashcard.field}
                     </div>
-                    ))
-                }
-                {flashcards.map((flashcard, index) => (
-                    <div 
-                    className="w-60 h-60 rounded-lg border-primary_blue border-4 flex justify-center items-center" 
-                    key={index}
-                    onClick={() => handleSum(flashcard.answer)}
-                >
-                    {flashcard.answer}
-                </div>
                     ))
                 }
             </div>
