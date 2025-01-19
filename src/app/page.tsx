4c5be44f155b839acd4866efc86d217eab7dd631
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-01-19 13:15:57 -0500

    Started implementing being able to add flashcard decks in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,35 +7,60 @@ import { useEffect, useState } from "react";
 import React from "react";
 import { useRouter } from "next/navigation";
 import AddFlashCardForm from "./components/addflashcard";
+import { FlashcardDeck } from "@prisma/client";
 
 export default function Home() {
   const router = useRouter();
   const [signedIn, setSignedIn] = useState(false);
+  const [flashcardDecks, setFlashcardDecks] = useState<FlashcardDeck[]>([]);
 
-  // useEffect(() => {
-  //   (async () => { 
-  //     const user = await getUser();
-  //     console.log("user", user);
+  useEffect(() => {
+    (async () => { 
+      const user = await getUser();
+      console.log("user", user);
 
-  //     if (!user) {
-  //       router.push("/signin")
-  //       return;
-  //     }
-  //     setSignedIn(true);
-  //   })()
-  // })
+      if (!user) {
+        router.push("/signin")
+        return;
+      }
+      setSignedIn(true);
+      const flashcardDecks = await getFlashcardDecks(user);
+      console.log(flashcardDecks);
 
-  // const getUser = async() => {
-  //   try {
-  //     const response = await fetch("/api/getSession");
-  //     const data = await response.json();
-  //     console.log("This is the token: ", data.token);
-  //     console.log("This is the email: ", data.token.email); 
-  //     return data.token.email
-  //   } catch (error) {
-  //     return null;
-  //   }
-  // }
+      if (!flashcardDecks) {
+        console.log("unable to get flashcard decks");
+      }
+
+      setFlashcardDecks(flashcardDecks);
+    })()
+  }, [])
+
+  const getUser = async() => {
+    try {
+      const response = await fetch("/api/getSession");
+      const data = await response.json();
+      console.log("This is the token: ", data.token);
+      console.log("This is the email: ", data.token.email); 
+      return data.token.email
+    } catch (error) {
+      return null;
+    }
+  }
+
+  const getFlashcardDecks = async(email : any) => {
+    try {
+      const response = await fetch(`/api/flashcardDecks?email=${email}`);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  const handleFlashcardDeckClick = (flashcardDeckID : string) => {
+    console.log(`render page with flashcards from flashcard deck with id of ${flashcardDeckID}`);
+  }
 
   const signout = async() => {
     try {
@@ -61,11 +86,17 @@ export default function Home() {
 
   if (signedIn) {
     return (
-      <div>
+      <div className="flex flex-col items-center justify-center gap-20">
         <Header />
-        You are signed in!
-        <AddFlashCardForm />
-        <button onClick={() => signout()}>Sign out</button>
+        <div className="grid grid-cols-4 gap-8">
+          {
+            flashcardDecks?.map((deck, index) => (
+              <div className="border-primary_blue border-2 px-4 py-4 flex justify-between items-center w-60 cursor-pointer rounded-md" key={index} onClick={() => handleFlashcardDeckClick(deck.id)}>
+                <p>{deck.name}</p>
+                <p>1</p>
+              </div>
+          ))}
+        </div>
       </div>
     );
   } else {
