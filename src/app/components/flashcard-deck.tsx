b5b3d53238d688d7c77bef33889c5d9be7bf9064
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-02-03 14:04:16 -0500

    Added ability to create a new flashcard and added dialog menu

--- src/app/components/flashcard-deck.tsx
+++ src/app/components/flashcard-deck.tsx
@@ -1,21 +1,60 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { 
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+    DialogFooter
+  } from "@/components/ui/dialog"
+  
 import { Flashcard } from "@prisma/client";
 import Trashcan from "../../../public/trash-can-small.png";
+import Plus from "../../../public/plus-button.png";
 import Image from "next/image";
 
 export default function FlashcardDeck({flashcardDeckID}: {flashcardDeckID: string}) {
     const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
-    const [question, setQuestion] = useState("");
-    const [answer, setAnswer] = useState("");
+    const [newQuestion, setNewQuestion] = useState("");
+    const [newAnswer, setNewAnswer] = useState("");
+
+    const createFlashcard = async(question: string, answer:string) => {
+        console.log(question);
+        console.log(answer);
+        try {
+            const response = await fetch("/api/addFlashCard", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    flashcardDeckID: flashcardDeckID,
+                    question: question,
+                    answer: answer
+                })
+            });
+    
+            if (!response.ok) {
+                const json = await response.json();
+                throw new Error(json.message);
+            }
+    
+            const json = await response.json();
+            console.log(json);
+            console.log(json.data);
+
+            setFlashcards([...flashcards, json.data]);
+            setNewQuestion("");
+            setNewAnswer("");
+
+            return json.data;
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
 
     const deleteFlashcard = async(flashcardID: string) => {
         console.log(flashcardID);
@@ -135,10 +174,52 @@ export default function FlashcardDeck({flashcardDeckID}: {flashcardDeckID: strin
 
     return (
         <div className="grid grid-cols-1 gap-8 w-1/3 mt-8">
+            <Dialog>
+                <DialogTrigger asChild>
+                <button className="flex justify-center items-center p-4 rounded-full">
+                    <Image src={Plus} alt="+" />
+                </button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle></DialogTitle>
+                        <DialogDescription>
+                            Create a new flashcard
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <label htmlFor="question" className="text-right">
+                            Question
+                            </label>
+                            <input
+                            id="question"
+                            className="col-span-3"
+                            value={newQuestion}
+                            onChange={(e) => setNewQuestion(e.target.value)}
+                            />
+                        </div>
+                        <div className="grid grid-cols-4 items-center gap-4">
+                            <label htmlFor="answer" className="text-right">
+                            Answer
+                            </label>
+                            <input
+                            id="answer"
+                            className="col-span-3"
+                            value={newAnswer}
+                            onChange={(e) => setNewAnswer(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <button onClick={() => createFlashcard(newQuestion, newAnswer)}>Save changes</button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
             {flashcards.map((flashcard, index) => (
                 <div className="border-primary_blue border-2 rounded-lg flex flex-col" key={index}>
                     <div className="flex justify-between items-center px-4 py-2">
-                        <p>{index}</p>
+                        <p className="font-bold">{index}</p>
                         <Image src={Trashcan} alt="trash" className="cursor-pointer" onClick={() => deleteFlashcard(flashcard.id)} />
                     </div>
                     <hr className="border-primary_blue h-1 w-full" />
