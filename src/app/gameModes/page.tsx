e095d2a8f26434cc2810693e0ec1c1d7306075a8
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-02-10 13:01:53 -0500

    Restyled game modes page

--- src/app/gameModes/page.tsx
+++ src/app/gameModes/page.tsx
@@ -1,16 +1,13 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { 
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel";
 import Header from "../components/header";
 import { Flashcard } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import ViewingIcon from "../../../public/viewingicon.png";
+import MatchingIcon from "../../../public/matchingicon.png";
+import LearningIcon from "../../../public/learningicon.png";
 
 export default function GameModes() {
     const router = useRouter();
@@ -18,15 +15,27 @@ export default function GameModes() {
     return (
         <div className="flex flex-col justify-center items-center gap-16">
             <Header />
-            <div className="flex gap-8">
-                <div className="w-[300px] h-[200px] border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer" onClick={() => router.push("/getFlashcards")}>
-                    Viewing
-                </div>
-                <div className="w-[300px] h-[200px] border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer" onClick={() => router.push("/learning-game")}>
-                    Learning
-                </div>
-                <div className="w-[300px] h-[200px] border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer" onClick={() => router.push("/matching-game")}>
-                    Matching
+            <div className="flex flex-col gap-8 justify-center items-center">
+                <h1 className="font-bold text-3xl">Game Modes</h1>
+                <div className="flex gap-16">
+                    <div className="flex flex-col justify-center items-center gap-4" onClick={() => router.push("/getFlashcards")}>
+                        <div className="border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer py-8 px-10 hover:shadow-lg">
+                            <Image src={MatchingIcon} alt="viewing" />
+                        </div>
+                        <p className="text-xl font-semibold">Matching</p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-4" onClick={() => router.push("/getFlashcards")}>
+                        <div className="border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer py-8 px-10 hover:shadow-lg">
+                            <Image src={LearningIcon} alt="viewing" />
+                        </div>
+                        <p className="text-xl font-semibold">Learning</p>
+                    </div>
+                    <div className="flex flex-col justify-center items-center gap-4" onClick={() => router.push("/getFlashcards")}>
+                        <div className="border-primary_blue border-2 rounded-lg flex justify-center items-center cursor-pointer py-8 px-10 hover:shadow-lg">
+                            <Image src={ViewingIcon} alt="viewing" />
+                        </div>
+                        <p className="text-xl font-semibold">Viewing</p>
+                    </div>
                 </div>
             </div>
         </div>
