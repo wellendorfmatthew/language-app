d0ff3135fbc5af2dd7564adc53f609fa89c870b8
Matthew Wellendorf <wellendorfmatthew@gmail.com>
2025-02-18 17:17:09 -0500

    Added ability to display shuffled flashcard questions and answers, handle true or false value, progress bar

--- src/app/true-false/page.tsx
+++ src/app/true-false/page.tsx
@@ -4,14 +4,16 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import { Flashcard } from "@prisma/client";
 import { Progress } from "@/components/ui/progress";
+import { Skeleton } from "@/components/ui/skeleton";
 import Image from "next/image";
 
 export default function TrueFalse() {
     const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+    const [answerDeck, setAnswerDeck] = useState<Flashcard[]>([]);
     const [progress, setProgress] = useState(10);
     const [facingFront, setFacingFront] = useState(true);
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [id, setID] = useState("");
+    const [id, setID] = useState(0);
     const [answer, setAnswer] = useState("");
 
     const getFlashcards = async() => {
@@ -32,6 +34,29 @@ export default function TrueFalse() {
         }
     }
 
+    const shuffleCards = (cards: Flashcard[]): Flashcard[] => {
+        const newArray = [...cards];
+    
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        }
+    
+        return newArray;
+    }
+
+    const handleAnswer = (answer: string, bool: boolean) => {
+        if (answer === flashcards[id].answer && bool === true) {
+            setID(id + 1);
+            setProgress(progress + 10);
+        } else if (answer !== flashcards[id].answer && bool === false){
+            setID(id + 1);
+            setProgress(progress + 10);
+        } else {
+            setID(id + 1);
+        }
+    }
+
     useEffect(() => {
     (async () => { 
         const flashcardDeck = await getFlashcards();
@@ -41,7 +66,20 @@ export default function TrueFalse() {
             console.log("Can't get cards");
         }
 
-        setFlashcards(flashcardDeck)
+        const shuffledDeck = shuffleCards(flashcardDeck);
+
+        console.log(shuffledDeck);
+        console.log(shuffledDeck[0]);
+        console.log(shuffledDeck[0].question);
+
+        const shuffledAnswers = shuffleCards(flashcardDeck);
+
+        console.log(shuffledAnswers);
+        console.log(shuffledAnswers[0]);
+        console.log(shuffledAnswers[0].question);
+
+        setFlashcards(shuffledDeck);
+        setAnswerDeck(shuffledAnswers);
     })()
     }, []);
 
@@ -51,12 +89,16 @@ export default function TrueFalse() {
              <div className="mt-12 flex flex-col justify-center items-center gap-8">
                 <h1 className="font-bold text-3xl">True or False</h1>
                 <Progress value={progress} className="h-8 w-80" />
-                <div className="flex items-center justify-center border-2 rounded-lg border-black w-80 h-52">
-                    <p className="font-bold">Bonjour translates to Hello</p>
-                </div>
+                {flashcards.length > 0 ? (
+                    <div className="flex items-center justify-center border-2 rounded-lg border-black w-80 h-52">
+                        <p className="font-bold">{flashcards[id].question} translates to {answerDeck[id].answer}?</p>
+                    </div>
+                ) : (
+                    <Skeleton className="w-80 h-52 rounded-lg" />
+                )}
                 <div className="flex items-center justify-center gap-8">
-                    <button className="bg-primary_blue rounded-full text-xl text-white py-2 px-6 w-24">True</button>
-                    <button className="bg-primary_red rounded-full text-xl text-white py-2 px-6 w-24">False</button>
+                    <button className="bg-primary_blue rounded-full text-xl text-white py-2 px-6 w-24" onClick={() => handleAnswer(answerDeck[id].answer, true)}>True</button>
+                    <button className="bg-primary_red rounded-full text-xl text-white py-2 px-6 w-24" onClick={() => handleAnswer(answerDeck[id].answer, false)}>False</button>
                 </div>
              </div>
         </div>
